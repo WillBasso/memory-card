a0f979914e1e87f9d241a37d010747048ff85605
WillBasso <willianalexbasso.wb@gmail.com>
2025-11-21 16:46:02 -0300

    Fiz deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const REPO_NAME = 'memory-card'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${REPO_NAME}/`,
   plugins: [react()],
 })
